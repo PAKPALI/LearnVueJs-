9790c02dca4ae1c5eba2cb3e3bc6242f495c02a6
Didier PAKPALI <pakpalididier@gmail.com>
2025-07-25 20:11:16 +0000

    work and finish modal front-end

--- resources/js/pages/Product.vue
+++ resources/js/pages/Product.vue
@@ -17,18 +17,20 @@
             </div>
             <div class="mb-3">
               <label for="quantite" class="form-label">Quantité</label>
-              <input type="number" class="form-control" id="quantite" v-model="quantite">
+              <input type="number" class="form-control" id="quantite" v-model="quantite" @keyup="validateQte">
+              <label :class="labelQteLog">{{ qteLog }}</label>
             </div>
             <div class="mb-3">
               <label for="limite" class="form-label">Limite</label>
-              <input type="number" class="form-control" id="limite" v-model="limite">
+              <input type="number" class="form-control" id="limite" v-model="limite" @keyup="validateLimit">
+              <label :class="labelLimitLog">{{ limitLog }}</label>
             </div>
             <div class="mb-3">
-              <label for="statut" class="form-label">Statut</label>
-              <select class="form-select" id="statut" v-model="statut">
+              <label for="statut" class="form-label">Statut (<label :class="labelStatusLog">{{ statusLog }}</label></label>)
+              <select class="form-select" id="statut" v-model="status" @change="changeStatus">
                 <option value="">-- Choisir un statut --</option>
-                <option value="disponible">Disponible</option>
-                <option value="indisponible">Indisponible</option>
+                <option value="1">Disponible</option>
+                <option value="2">Indisponible</option>
               </select>
             </div>
           </form>
@@ -83,11 +85,23 @@
   const nom = ref('')
   const quantite = ref(0)
   const limite = ref(0)
-  const statut = ref('')
+
   const nameLog = ref('')
   const labelNameLog = ref('text-danger')
   let nameLenght = 0
 
+  const qteLog = ref('La quantité est égale à 0')
+  const labelQteLog = ref('text-danger')
+  let qte = 0
+
+  const limitLog = ref('La limite est égale à 0')
+  const labelLimitLog = ref('text-danger')
+  let limit = 0
+
+  const statusLog = ref('Le status est sur indisponible')
+  const labelStatusLog = ref('text-danger')
+  const status = ref('')
+  
   function addProduct() {
     // Ouvre le modal pour ajouter un produit
     const modal = new Modal(document.getElementById('addProductModal'))
@@ -98,5 +112,31 @@
     nameLenght = event.target.value.length
     nameLog.value = nameLenght< 3 ? 'Le nom doit contenir au moins 3 caractères' : 'Nombre de caractère valide'
     labelNameLog.value = nameLenght < 3 ? 'text-danger' : 'text-success'
+  }
+
+  function validateQte(event) {
+    qte = event.target.value
+    qteLog.value = qte <= 0 ? 'La quantité est égale à 0' : 'La quantité est égale à ' + qte
+    labelQteLog.value = qte <= 0 ? 'text-danger' : 'text-success'
+  }
+
+  function validateLimit(event) {
+    limit = event.target.value
+    limitLog.value = limit <= 0 ? 'La limite est égale à 0' : 'La limite est égale à ' + limit
+    labelLimitLog.value = limit <= 0 ? 'text-danger' : 'text-success'
+  }
+
+  function changeStatus(event) {
+    status.value = event.target.value
+    if(status.value == 0 ) {
+      statusLog.value = 'Choisir un statut'
+      labelStatusLog.value = 'text-danger'
+    }else if (status.value == 1) {
+      statusLog.value = 'Le status est disponible'
+      labelStatusLog.value = 'text-success'
+    }else {
+      statusLog.value = 'Le status est indisponible'
+      labelStatusLog.value = 'text-danger'
+    }
   }
 </script>
